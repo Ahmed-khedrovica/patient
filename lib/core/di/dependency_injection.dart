8b085bbf297c836e12c7b0019991ca9ffc0afb07
Your Name <you@example.com>
2026-03-29 23:49:47 +0200

    - registered specialties di.

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,12 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../features/home/data/repos/specialties_repo.dart';
+import '../../features/home/logic/specialities_cubit.dart';
 import '../../features/login/data/repos/login_repo.dart';
 import '../../features/login/logic/login_cubit.dart';
 import '../../features/profile/data/repos/profile_repo.dart';
 import '../../features/profile/logic/profile_cubit.dart';
 import '../../features/sign_up/data/repos/signup_repo.dart';
 import '../../features/sign_up/logic/signup_cubit.dart';
+import '../helpers/token_manger.dart';
 import '../networking/api_service.dart';
 import '../networking/dio_factory.dart';
 
@@ -17,9 +20,12 @@ Future<void> setupGetIt() async {
   Dio dio = DioFactory.getDio();
   getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
 
+  // Token manager
+  getIt.registerLazySingleton<TokenManager>(() => TokenManager());
+
   // login
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
-  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt(), getIt()));
 
   //  signup
   getIt.registerLazySingleton<SignupRepo>(() => SignupRepo(getIt()));
@@ -28,4 +34,8 @@ Future<void> setupGetIt() async {
   // profile
   getIt.registerLazySingleton<ProfileRepo>(() => ProfileRepo(getIt()));
   getIt.registerFactory<ProfileCubit>(() => ProfileCubit(getIt()));
+
+  // specialties
+  getIt.registerLazySingleton<SpecialtiesRepo>(() => SpecialtiesRepo(getIt()));
+  getIt.registerFactory<SpecialitiesCubit>(() => SpecialitiesCubit(getIt()));
 }
